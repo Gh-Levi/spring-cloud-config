9b02cd43012613112b6e2e1e0e897138c04447ac
levi.gong <levi.gong@sinoservices.com>
2019-12-12 18:05:32 +0800

    init push

---
+++ config-client/src/test/java/io/levi/baeldung/demo/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.levi.baeldung.demo.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
